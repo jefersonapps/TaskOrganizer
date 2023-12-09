79a765c52a1633e53572fb8b4f697afcde56d1d8
jefersonapps <jefersonplaynunes@gmail.com>
2023-12-09 18:43:06 -0300

    LitLens camera screen updated with new features

--- screens/lit-lens/CameraScreen.tsx
+++ screens/lit-lens/CameraScreen.tsx
@@ -1,32 +1,60 @@
 import { useContext, useEffect, useRef, useState } from "react";
-import { View, TouchableOpacity, StyleSheet } from "react-native";
+import {
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  TouchableNativeFeedback,
+} from "react-native";
 import {
   Camera,
+  CameraPosition,
   useCameraDevices,
   useFrameProcessor,
 } from "react-native-vision-camera";
 import { useCameraPermission } from "../../Hooks/usePermission";
+import { MaterialIcons, Ionicons } from "@expo/vector-icons";
+
 import { GetPermission } from "../../components/GetPermission";
-import { runOnJS } from "react-native-reanimated";
+import {
+  runOnJS,
+  useAnimatedGestureHandler,
+  useSharedValue,
+} from "react-native-reanimated";
 import { AppContext } from "../../contexts/AppContext";
 import { scanOCR } from "vision-camera-ocr";
 import { useIsFocused } from "@react-navigation/native";
 import { useNavigation } from "@react-navigation/native";
+import {
+  PinchGestureHandler,
+  PinchGestureHandlerGestureEvent,
+  TapGestureHandler,
+  TapGestureHandlerGestureEvent,
+} from "react-native-gesture-handler";
+import { useAppTheme } from "../../theme/Theme";
+import { Button, Dialog, Paragraph, Portal } from "react-native-paper";
 
 export const CameraScreen = () => {
   const camera = useRef<Camera>(null);
   const devices = useCameraDevices();
-  const device = devices.back;
+  const theme = useAppTheme();
+
   const { cameraPermission, requestCameraPermission } = useCameraPermission();
   const { setImageSource, imageSource, setOcrResult } = useContext(AppContext);
   const [showCamera, setShowCamera] = useState(true);
+  const [cameraPosition, setCameraPosition] = useState<CameraPosition>("back");
 
   const navigation = useNavigation();
   const isFocused = useIsFocused();
 
+  const device = cameraPosition === "front" ? devices.front : devices.back;
+  const [isflashOn, setIsFlashOn] = useState<"off" | "on">("off");
+  const [isFlashAlerVisible, setIsFlashAlertVisible] = useState(false);
+
   useEffect(() => {
     if (!isFocused) {
       setShowCamera(false);
+    } else {
+      setShowCamera(true);
     }
   }, [isFocused]);
 
@@ -50,6 +78,53 @@ export const CameraScreen = () => {
     [imageSource]
   );
 
+  const [zoom, setZoom] = useState(0);
+
+  const pinchHandler = (e: PinchGestureHandlerGestureEvent) => {
+    if (e.nativeEvent.scale !== undefined) {
+      setFocusPoint(null);
+      setZoom(e.nativeEvent.scale);
+    }
+  };
+
+  const [focusPoint, setFocusPoint] = useState<{ x: number; y: number } | null>(
+    null
+  );
+
+  const isFocusing = useRef(false);
+
+  const handleTap = async (event: TapGestureHandlerGestureEvent) => {
+    if (camera.current && !isFocusing.current) {
+      isFocusing.current = true;
+      setFocusPoint({ x: event.nativeEvent.x, y: event.nativeEvent.y });
+      try {
+        if (event.nativeEvent.x > 330 && event.nativeEvent.y < 130) return;
+        if (event.nativeEvent.y > 630) return;
+        await camera.current.focus({
+          x: event.nativeEvent.x,
+          y: event.nativeEvent.y,
+        });
+      } catch (error) {
+        console.log(error);
+      } finally {
+        isFocusing.current = false;
+        setFocusPoint(null);
+      }
+    }
+  };
+
+  const toggleCamera = () => {
+    setCameraPosition((prevState) => (prevState === "back" ? "front" : "back"));
+  };
+
+  const toggleFlash = () => {
+    if (device === devices.back) {
+      setIsFlashOn((prevFlash) => (prevFlash === "off" ? "on" : "off"));
+    } else {
+      setIsFlashAlertVisible(true);
+    }
+  };
+
   if (cameraPermission === "denied" || !device) {
     return (
       <GetPermission
@@ -62,22 +137,103 @@ export const CameraScreen = () => {
     );
   }
   return (
-    <>
-      <Camera
-        ref={camera}
-        style={StyleSheet.absoluteFill}
-        device={device}
-        isActive={showCamera}
-        photo={true}
-        frameProcessor={frameProcessor}
-      />
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity
-          style={styles.camButton}
-          onPress={() => capturePhoto()}
-        ></TouchableOpacity>
-      </View>
-    </>
+    <TapGestureHandler onHandlerStateChange={handleTap}>
+      <PinchGestureHandler onGestureEvent={pinchHandler}>
+        <View style={{ flex: 1, position: "relative" }}>
+          <View
+            style={{
+              position: "absolute",
+              top: 14,
+              right: 14,
+              zIndex: 1,
+              gap: 8,
+            }}
+          >
+            <Portal>
+              <Dialog
+                visible={!!isFlashAlerVisible}
+                onDismiss={() => setIsFlashAlertVisible(false)}
+              >
+                <Dialog.Title>Flash Indisponível</Dialog.Title>
+                <Dialog.Content>
+                  <Paragraph>
+                    Desculpe, a funcionalidade de flash não está disponível para
+                    a câmera frontal do dispositivo.
+                  </Paragraph>
+                </Dialog.Content>
+                <Dialog.Actions>
+                  <Button onPress={() => setIsFlashAlertVisible(false)}>
+                    Ok
+                  </Button>
+                </Dialog.Actions>
+              </Dialog>
+            </Portal>
+
+            <TouchableOpacity onPress={toggleFlash} activeOpacity={0.6}>
+              <View
+                style={{
+                  padding: 14,
+                  borderRadius: 9999,
+                  backgroundColor: theme.colors.inversePrimary,
+                }}
+              >
+                <MaterialIcons
+                  name={isflashOn === "on" ? "flash-on" : "flash-off"}
+                  size={24}
+                  color={theme.colors.onSurface}
+                />
+              </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={toggleCamera} activeOpacity={0.6}>
+              <View
+                style={{
+                  padding: 14,
+                  borderRadius: 9999,
+                  backgroundColor: theme.colors.inversePrimary,
+                }}
+              >
+                <Ionicons
+                  name="ios-camera-reverse-sharp"
+                  size={24}
+                  color={theme.colors.onSurface}
+                />
+              </View>
+            </TouchableOpacity>
+          </View>
+          <Camera
+            ref={camera}
+            torch={isflashOn}
+            style={StyleSheet.absoluteFill}
+            device={device}
+            isActive={showCamera}
+            photo={true}
+            frameProcessor={frameProcessor}
+            zoom={zoom}
+          />
+          {focusPoint && (
+            <View
+              style={{
+                position: "absolute",
+                left: focusPoint.x - 25,
+                top: focusPoint.y - 25,
+                width: 50,
+                height: 50,
+                borderRadius: 25,
+                borderWidth: 2,
+                borderColor: "white",
+              }}
+            />
+          )}
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              style={styles.camButton}
+              onPress={() => capturePhoto()}
+            ></TouchableOpacity>
+          </View>
+        </View>
+      </PinchGestureHandler>
+    </TapGestureHandler>
   );
 };
 
